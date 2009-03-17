eb7db9061f3fe87e7eab29830542bb60e45cef10
Marcin Konecki <sha1-3dd4037a88237b8e967681abdc78f7c551f4f0d0@cern.ch>
2009-03-17 00:13:54 +0000

    *** empty log message ***

--- test/r2d.py
+++ test/r2d.py
@@ -5,14 +5,14 @@ process.load("EventFilter.RPCRawToDigi.RPCSQLiteCabling_cfi")
 
 process.load("EventFilter.RPCRawToDigi.rpcUnpacker_cfi")
 
-process.load("DQM.RPCMonitorModule.rpcMonitorRaw_cfi")
-
 process.load("DQMServices.Core.DQM_cfg")
 
 # set maxevents; -1 -> take all
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(300))
 
-process.source = cms.Source ("PoolSource",fileNames = cms.untracked.vstring( 'file:r067647.root') )
+process.source = cms.Source ("PoolSource",fileNames = cms.untracked.vstring(
+'/store/data/Commissioning08/Cosmics/RAW/v1/000/070/036/86276C9B-72AD-DD11-90F9-000423D6C8EE.root'
+))
 
 # correct output file
 process.MessageLogger = cms.Service("MessageLogger",
@@ -21,7 +21,5 @@ process.MessageLogger = cms.Service("MessageLogger",
     cout = cms.untracked.PSet( threshold = cms.untracked.string('INFO'))
 )
 
-process.rpcMonitorRaw.writeHistograms = True
-
-process.p = cms.Path(process.rpcunpacker*process.rpcMonitorRaw)
+process.p = cms.Path(process.rpcunpacker)
 
